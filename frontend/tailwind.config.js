f5c61372ac309974c2eec33d43d6178dbe8dbbbb
JaGarcia02 <jagarcia02051998@gmail.com>
2023-12-28 14:24:31 +0800

    screen pc resolution has been fixed in the responsiveness of login page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      monitor_xsm: "1024px",
+      monitor_sm: "1280px",
+      monitor_md: "1366px",
+      monitor_xl: "1440px",
+      monitor_xxl: "1680px",
+    },
   },
   plugins: [],
 };
