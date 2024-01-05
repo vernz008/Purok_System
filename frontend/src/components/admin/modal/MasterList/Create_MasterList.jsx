ea287830253e7095ddb4d32b6cbd7c7937d2d29d
Ja Garcia <jagarcia02051998@gmail.com>
2024-01-05 18:38:23 +0800

    add member modal optimized screen resolution for 1366

--- frontend/src/components/admin/modal/MasterList/Create_MasterList.jsx
+++ frontend/src/components/admin/modal/MasterList/Create_MasterList.jsx
@@ -8,44 +8,94 @@ const Create_MasterList = ({
   org_data,
 }) => {
   return (
-    <div className="absolute !top-0 !left-0 w-screen h-screen z-999 bg-black/50 flex justify-center items-center">
-      <div className="h-[75%] w-[65%] bg-white rounded-md">
+    <div className="absolute !top-0 !left-0 w-screen h-screen z-999 bg-black/50 flex justify-center items-center overflow-hidden">
+      <div
+        className="h-[75%] w-[65%] bg-white rounded-md
+      monitor_md:h-[75%]
+      monitor_md:w-[60%]
+      "
+      >
         {/* Headers */}
-        <div className="w-full h-[6.5rem] flex justify-between items-center bg-blue-600 rounded-tl-md rounded-tr-md">
+        <div
+          className="w-full h-[6.5rem] flex justify-between items-center bg-blue-600 rounded-tl-md rounded-tr-md
+        monitor_md:h-[4rem]
+        "
+        >
           <span className="w-[90%] px-5">
-            <p className="text-white text-[25px] underline font-bold">
+            <p
+              className="text-white text-[25px] underline font-bold
+            monitor_md:text-[20px]
+            "
+            >
               Masterlist
             </p>
-            <p className="text-white text-[16px] font-semibold">Add Member</p>
+            <p
+              className="text-white text-[16px] font-semibold
+            monitor_md:text-[14px]
+            "
+            >
+              Add Member
+            </p>
           </span>
           <div className="h-full w-[5%] flex justify-end items-start p-2">
             <button
               className="bg-white border-[1.5px] border-red-500 rounded-full transition-all ease-in-out duration-300 text-red-500 hover:text-red-400"
               onClick={() => setMasterList_Modal(false)}
             >
-              <FaTimesCircle className="text-[25px] " />
+              <FaTimesCircle
+                className="text-[25px]
+              monitor_md:text-[22px]
+              "
+              />
             </button>
           </div>
         </div>
         {/* Body */}
-        <div className="w-full h-[85%] p-5">
+        <div
+          className="w-full h-[85%] p-5
+        monitor_md:p-2
+        monitor_md:px-5
+        "
+        >
           <form action="" className="w-full">
             <div className="w-full">
               <div className="w-full">
-                <fieldset className="border-[2px] px-10 h-[8.5rem] rounded-md">
-                  <legend className="text-[18px] font-bold">Assignment</legend>
+                <fieldset
+                  className="border-[2px] px-10 h-[8.5rem] rounded-md
+                monitor_md:h-[5.5rem]
+                "
+                >
+                  <legend
+                    className="text-[18px] font-bold
+                  monitor_md:text-[16px]
+                  "
+                  >
+                    Assignment
+                  </legend>
 
                   {/* Assignments */}
-                  <div className="w-full h-[5.5rem] flex justify-start items-center">
+                  <div
+                    className="w-full h-[5.5rem] flex justify-start items-center
+                  monitor_md:h-[2.8rem]
+                  "
+                  >
                     <div className="flex flex-col w-[37%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                      "
+                      >
                         Organization
                       </label>
                       <select
                         name=""
                         id=""
                         required
-                        className="border-[1px] shadow-md shadow-slate-400 w-[67%] h-[2.5rem] outline-none cursor-pointer text-[14px] px-1"
+                        className="border-[1px] shadow-md shadow-slate-400 w-[67%] h-[2.5rem] outline-none cursor-pointer text-[14px] px-1
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       >
                         <option value="" disabled selected>
                           Select Organization . . . .
@@ -62,14 +112,22 @@ const Create_MasterList = ({
                       </select>
                     </div>
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                      "
+                      >
                         Purok
                       </label>
                       <select
                         name=""
                         id=""
                         required
-                        className="border-[1px] shadow-md shadow-slate-400 w-full h-[2.5rem] outline-none cursor-pointer text-[14px] px-1"
+                        className="border-[1px] shadow-md shadow-slate-400 w-full h-[2.5rem] outline-none cursor-pointer text-[14px] px-1
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       >
                         <option value="" disabled selected>
                           Select Purok . . . .
@@ -110,50 +168,95 @@ const Create_MasterList = ({
                   {/* Assignments */}
                 </fieldset>
 
-                <fieldset className="border-[2px] px-10 h-[14rem] rounded-md">
-                  <legend className="text-[18px] font-bold">
+                <fieldset
+                  className="border-[2px] px-10 h-[14rem] rounded-md
+                monitor_md:h-[8.5rem] 
+                "
+                >
+                  <legend
+                    className="text-[18px] font-bold
+                  monitor_md:text-[16px]
+                  "
+                  >
                     Personal Info
                   </legend>
                   {/* Full name */}
-                  <div className="w-full h-[5.5rem] flex justify-between items-center">
+                  <div
+                    className="w-full h-[5.5rem] flex justify-between items-center
+                  monitor_md:h-[3rem]
+                  "
+                  >
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                      "
+                      >
                         Firstname
                       </label>
                       <input
                         type="text"
                         required
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                      "
+                      >
                         Middlename
                       </label>
                       <input
                         type="text"
                         required
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                      "
+                      >
                         Lastname
                       </label>
                       <input
                         type="text"
                         required
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                   </div>
                   {/* Full name */}
 
-                  <div className="flex w-full">
+                  <div
+                    className="flex w-full
+                  monitor_md:mt-1
+                  "
+                  >
                     {/* Birthdate */}
                     <div className="w-[50%]">
                       <div className="flex flex-col">
-                        <label htmlFor="" className="text-[16px] font-semibold">
+                        <label
+                          htmlFor=""
+                          className="text-[16px] font-semibold
+                          monitor_md:text-[14px]
+                        "
+                        >
                           Birth Date
                         </label>
                         <input
@@ -161,7 +264,10 @@ const Create_MasterList = ({
                           name=""
                           id=""
                           required
-                          className="h-[2.5rem] w-[66%] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                          className="h-[2.5rem] w-[66%] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                          monitor_md:h-[1.5rem]
+                          monitor_md:text-[12px]
+                          "
                         />
                       </div>
                     </div>
@@ -170,14 +276,22 @@ const Create_MasterList = ({
                     {/* Gender */}
                     <div className="w-[83%]">
                       <div className="flex flex-col">
-                        <label htmlFor="" className="text-[16px] font-semibold">
+                        <label
+                          htmlFor=""
+                          className="text-[16px] font-semibold
+                        monitor_md:text-[14px]
+                        "
+                        >
                           Gender
                         </label>
                         <select
                           name=""
                           id=""
                           required
-                          className="border-[1px] shadow-md shadow-slate-400 w-[40%] h-[2.5rem] outline-none cursor-pointer text-[14px] px-1"
+                          className="border-[1px] shadow-md shadow-slate-400 w-[40%] h-[2.5rem] outline-none cursor-pointer text-[14px] px-1
+                          monitor_md:h-[1.5rem]
+                          monitor_md:text-[12px]
+                          "
                         >
                           <option value="" selected disabled>
                             Select Gender . . . .
@@ -193,49 +307,94 @@ const Create_MasterList = ({
                   </div>
                 </fieldset>
 
-                <fieldset className="border-[2px] px-10 h-[8.5rem] rounded-md">
-                  <legend className="text-[18px] font-bold">Address</legend>
+                <fieldset
+                  className="border-[2px] px-10 h-[8.5rem] rounded-md
+                monitor_md:h-[5.2rem]
+                "
+                >
+                  <legend
+                    className="text-[18px] font-bold
+                  monitor_md:text-[16px]
+                  "
+                  >
+                    Address
+                  </legend>
 
                   {/* Address */}
-                  <div className="w-full h-[5.5rem] flex justify-between items-center">
+                  <div
+                    className="w-full h-[5.5rem] flex justify-between items-center
+                  monitor_md:h-[2.6rem]
+                  "
+                  >
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                         monitor_md:text-[14px]
+                      "
+                      >
                         Street
                       </label>
                       <input
                         type="text"
                         required
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                         monitor_md:text-[14px]
+                      "
+                      >
                         Barangay
                       </label>
                       <input
                         type="text"
                         required
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                     <div className="flex flex-col w-[25%]">
-                      <label htmlFor="" className="text-[16px] font-semibold">
+                      <label
+                        htmlFor=""
+                        className="text-[16px] font-semibold
+                         monitor_md:text-[14px]
+                      "
+                      >
                         Municipality
                       </label>
                       <input
                         type="text"
-                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]"
+                        className="h-[2.2rem] shadow-md shadow-slate-400 outline-none px-2 border-[1px] text-[14px]
+                        monitor_md:h-[1.5rem]
+                        monitor_md:text-[12px]
+                        "
                       />
                     </div>
                   </div>
                   {/*Address */}
                 </fieldset>
               </div>
-              <div className="w-full h-[2.5rem] flex justify-center items-center mt-5">
+              <div
+                className="w-full h-[2.5rem] flex justify-center items-center mt-5
+              monitor_md:mt-4
+              monitor_md:h-[2rem]
+              "
+              >
                 <button
                   type="submit"
                   required
-                  className="w-[20%] h-full bg-blue-500 transition-all duration-300 text-white font-bold text-[18px] rounded-md hover:bg-blue-400"
+                  className="w-[20%] h-full bg-blue-500 transition-all duration-300 text-white font-bold text-[18px] rounded-md hover:bg-blue-400
+                  monitor_md:text-[16px]
+                  "
                 >
                   Submit
                 </button>
