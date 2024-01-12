dc7c21eda0133bbc05d899bcec91157cbdf94ce6
Ja Garcia <jagarcia02051998@gmail.com>
2024-01-12 22:38:13 +0800

    95% on update function in masterlist

--- frontend/src/components/admin/content/Masterlist_Content.jsx
+++ frontend/src/components/admin/content/Masterlist_Content.jsx
@@ -231,6 +231,7 @@ const Masterlist_Content = ({
         <Update_MasterList
           setUpdate_MasterList_Modal={setUpdate_MasterList_Modal}
           update_masterList_Modal={update_masterList_Modal}
+          setMasterList_Data={setMasterList_Data}
           organization_data={organization_data}
           purok_data={purok_data}
           group_data={group_data}
