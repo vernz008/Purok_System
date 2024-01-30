4eb02727ea233e9e69b73e85d5f47caf1a171729
Ja Garcia <jagarcia02051998@gmail.com>
2024-01-30 16:11:10 +0800

    fix sidebar

--- frontend/src/components/admin/sidebar/Sidebar.jsx
+++ frontend/src/components/admin/sidebar/Sidebar.jsx
@@ -137,8 +137,8 @@ const Sidebar = ({
       {/* Sidebar Function Buttons */}
       <div
         className={`w-full 
-      monitor_md:h-[85%]
-      ${logout_button.trigger === true ? " monitor_md:h-[90%]" : ""}
+      monitor_md:h-[25rem]
+     
       `}
       >
         {/* 
@@ -148,13 +148,17 @@ const Sidebar = ({
         
         
         */}
-        <div className="w-full h-[10%] flex justify-center items-center">
+        <div
+          className="w-full flex justify-center items-center
+        monitor_md:h-[4rem]
+        "
+        >
           <div
             className={`${
               sidebar_toggle === false
                 ? "w-[90%] h-[70%] flex justify-between items-center"
                 : "w-[90%] h-[60%] flex justify-between items-center transition ease-in-out rounded-md duration-300 hover:bg-blue-300"
-            } monitor_md:h-[90%] monitor_md:w-[80%]`}
+            } monitor_md:h-[60%] monitor_md:w-[80%]`}
           >
             <button
               disabled={sidebar_buttons.assignments}
@@ -215,13 +219,17 @@ const Sidebar = ({
         
         
         */}
-        <div className="w-full h-[10%] flex justify-center items-center">
+        <div
+          className="w-full flex justify-center items-center
+          monitor_md:h-[4rem]
+        "
+        >
           <div
             className={`${
               sidebar_toggle === false
                 ? "w-[90%] h-[70%] flex justify-between items-center"
                 : "w-[90%] h-[60%] flex justify-between items-center transition ease-in-out rounded-md duration-300 hover:bg-blue-300"
-            } monitor_md:h-[90%] monitor_md:w-[80%]`}
+            } monitor_md:h-[60%] monitor_md:w-[80%]`}
           >
             <button
               disabled={sidebar_buttons.masterlist}
@@ -289,13 +297,17 @@ const Sidebar = ({
 
         
         */}
-        <div className="w-full h-[10%] flex justify-center items-center">
+        <div
+          className="w-full flex justify-center items-center
+          monitor_md:h-[4rem]
+        "
+        >
           <div
             className={`${
               sidebar_toggle === false
                 ? "w-[90%] h-[70%] flex justify-between items-center"
                 : "w-[90%] h-[60%] flex justify-between items-center transition ease-in-out rounded-md duration-300 hover:bg-blue-300"
-            } monitor_md:h-[90%] monitor_md:w-[80%]`}
+            } monitor_md:h-[60%] monitor_md:w-[80%]`}
           >
             <button
               disabled={sidebar_buttons.masterlist}
@@ -363,72 +375,69 @@ const Sidebar = ({
       
       
       */}
-      {logout_button.trigger === true ? (
-        <div className="w-full h-[10%] flex justify-center items-center">
-          <div
-            className={`${
-              sidebar_toggle === false
-                ? "w-[90%] h-[70%] flex justify-between items-center"
-                : "w-[90%] h-[60%] flex justify-between items-center transition ease-in-out rounded-md duration-300 hover:bg-blue-300"
-            } monitor_md:h-[80%] monitor_md:w-[80%]`}
-          >
-            <button
-              onClick={() => {
-                axiosClient
-                  .post(API_LOGOUT)
-                  .then((res) => {
-                    alert("Logout  Successfully");
-                    Cookies.remove("access_token");
-                    navigate("/");
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                  });
-              }}
-              className={`
-              ${
-                sidebar_buttons.masterlist === true
-                  ? "rounded-md bg-gray-300 opacity-80"
-                  : ""
-              }
+
+      <div
+        className="w-full flex justify-center items-center
+        monitor_md:h-[4rem]
+        "
+      >
+        <div
+          className={`${
+            sidebar_toggle === false
+              ? "w-[90%] h-[70%] flex justify-between items-center"
+              : "w-[90%] h-[60%] flex justify-between items-center transition ease-in-out rounded-md duration-300 hover:bg-blue-300"
+          } monitor_md:h-[60%] monitor_md:w-[80%]`}
+        >
+          <button
+            onClick={() => {
+              axiosClient
+                .post(API_LOGOUT)
+                .then((res) => {
+                  alert("Logout  Successfully");
+                  Cookies.remove("access_token");
+                  navigate("/");
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            }}
+            className={`
               ${
                 sidebar_toggle === false
                   ? "flex justify-center items-center w-full h-full"
                   : "flex justify-between items-center w-full h-[2.5rem] font-extrabold"
               } disabled:cursor-not-allowed`}
+          >
+            <span
+              className={`${
+                sidebar_toggle === false
+                  ? "w-[80%] h-[90%] flex justify-center items-center transition ease-in-out rounded-full duration-300 hover:bg-blue-300"
+                  : "w-[20%] flex justify-center items-center h-full"
+              } `}
             >
-              <span
-                className={`${
-                  sidebar_toggle === false
-                    ? "w-[80%] h-[90%] flex justify-center items-center transition ease-in-out rounded-full duration-300 hover:bg-blue-300"
-                    : "w-[20%] flex justify-center items-center h-full"
-                } `}
-              >
-                <IoMdLogOut
-                  className="text-[25px]
+              <IoMdLogOut
+                className="text-[25px]
               monitor_md:text-[20px]
 
               monitor_xxl:text-[25px]
               "
-                />
-              </span>
-              <p
-                className={`${
-                  sidebar_toggle === false
-                    ? "hidden"
-                    : " w-[75%] text-[20px] flex items-center h-full"
-                } 
+              />
+            </span>
+            <p
+              className={`${
+                sidebar_toggle === false
+                  ? "hidden"
+                  : " w-[75%] text-[20px] flex items-center h-full"
+              } 
                 monitor_md:text-[16px]
                 `}
-              >
-                Logout
-              </p>
-            </button>
-          </div>
+            >
+              Logout
+            </p>
+          </button>
         </div>
-      ) : (
-        ""
-      )}
+      </div>
+
       {/* 
       
       
@@ -455,12 +464,6 @@ const Sidebar = ({
             <img
               src={PurokLogoImg}
               alt=""
-              onClick={() => {
-                setLogout_Button({ ...logout_button, trigger: true });
-                logout_button.trigger === true
-                  ? setLogout_Button({ ...logout_button, trigger: false })
-                  : setLogout_Button({ ...logout_button, trigger: true });
-              }}
               className={`cursor-pointer ${
                 sidebar_toggle === false
                   ? "w-[3.5rem] bg-white rounded-full"
