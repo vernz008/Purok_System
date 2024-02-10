3e93112c8b84cc534bddb0c86e8f9dbb7f665705
Ja Garcia <jagarcia02051998@gmail.com>
2024-02-10 22:43:46 +0800

    update the create attendance pop-up modal

--- frontend/src/components/admin/modal/Attendance/Update_Attendance.jsx
+++ frontend/src/components/admin/modal/Attendance/Update_Attendance.jsx
@@ -29,25 +29,21 @@ const Update_Attendance = ({
     useState(false);
   const [attendance_title, setAttendance_Title] = useState("");
   const [attendance_schedule, setAttendance_Schedule] = useState("");
-  const [attendance_id, setAttendance_Id] = useState(0);
+  // const [attendance_id, setAttendance_Id] = useState(0);
   const [user_data, setUser_Data] = useState([]);
-  const [member_data, setMember_Data] = useState([]);
-  const [record_data, setRecord_Data] = useState([]);
+  // const [member_data, setMember_Data] = useState([]);
+  // const [record_data, setRecord_Data] = useState([]);
   const [update_trigger, setUpdate_Trigger] = useState(false);
   const [update_trigger_schedule, setUpdate_Trigger_Schedule] = useState(false);
   const [button_attend_member, setButton_Attend_Member] = useState({
     loading: false,
     id: 0,
   });
-  const [record_member_data, setRecord_Member_Data] = useState({
-    member_count: 0,
-    member_attended: 0,
-    member_attended_list: [],
-  });
-
-  console.log(record_data);
-
-  console.log(member_data);
+  // const [record_member_data, setRecord_Member_Data] = useState({
+  //   member_count: 0,
+  //   member_attended: 0,
+  //   member_attended_list: [],
+  // });
 
   //  .filter((val, index, self) => {
   //     index ===
@@ -72,44 +68,35 @@ const Update_Attendance = ({
       .then((res) => {
         setAttendance_Title(res.data.pamagat);
         setAttendance_Schedule(res.data.schedule);
-        setAttendance_Id(res.data.id);
+        // setAttendance_Id(res.data.id);
         setLoading_Data(false);
       })
       .catch((error) => {
         console.log(error);
       });
 
-    axiosClient
-      .get(API_RECORD)
-      .then((res) => {
-        const total_members_count = res.data.length;
-        const total_attendee = res.data.filter(
-          (fil) => fil.attendance_Id === attendance_id
-        ).length;
-        const total_attendee_list = res.data.filter(
-          (val) => val.attendance_Id === attendance_id
-        );
+    // axiosClient
+    //   .get(API_RECORD)
+    //   .then((res) => {
+    //     const total_members_count = res.data.length;
+    //     const total_attendee = res.data.filter(
+    //       (fil) => fil.attendance_Id === attendance_id
+    //     ).length;
+    //     const total_attendee_list = res.data.filter(
+    //       (val) => val.attendance_Id === attendance_id
+    //     );
 
-        setRecord_Data(res.data);
-        setRecord_Member_Data({
-          ...record_member_data,
-          member_attended: total_attendee,
-          member_count: total_members_count,
-          member_attended_list: total_attendee_list,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    axiosClient
-      .get(API_MEMBER)
-      .then((res) => {
-        setMember_Data(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    //   setRecord_Data(res.data);
+    //   setRecord_Member_Data({
+    //     ...record_member_data,
+    //     member_attended: total_attendee,
+    //     member_count: total_members_count,
+    //     member_attended_list: total_attendee_list,
+    //   });
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
   }, []);
 
   return (
@@ -155,8 +142,8 @@ const Update_Attendance = ({
             <>
               <div
                 className="flex justify-center items-center w-full
-              monitor_md:h-[28.9rem]
-              "
+                monitor_md:h-[14rem]
+                "
               >
                 <span>
                   <AiOutlineLoading3Quarters className="animate-spin text-[2.5rem]" />
@@ -169,7 +156,7 @@ const Update_Attendance = ({
                 className="h-full flex justify-center items-center
                   monitor_md:py-5
                   monitor_md:w-[95%]
-              "
+                  "
               >
                 <div
                   className=" h-full 
@@ -386,6 +373,8 @@ const Update_Attendance = ({
                       onSubmit={(e) => {
                         e.preventDefault();
 
+                        setLoading_Trigger_Update_Schedule(true);
+
                         axiosClient
                           .put(API_ATTENDANCE + `/${modal_update.id}`, {
                             pamagat: attendance_title,
@@ -396,6 +385,8 @@ const Update_Attendance = ({
                             setAttendance_Data(res.data);
                             setLoading_Trigger_Update_Schedule(false);
                             setUpdate_Trigger_Schedule(false);
+
+                            setUpdate_Trigger(false);
                           })
                           .catch((error) => {
                             console.log(error.response.status);
@@ -408,94 +399,94 @@ const Update_Attendance = ({
                           "
                     >
                       <div className="w-full h-full flex">
-                        {update_trigger_schedule === true ? (
-                          <input
-                            type="datetime-local"
-                            required
-                            onChange={(e) =>
-                              setAttendance_Schedule(e.target.value)
-                            }
-                            value={attendance_schedule}
-                            className="border-[1px] outline-none shadow-md shadow-slate-300
+                        {loading_trigger_update_schedule === true ? (
+                          <div
+                            className="h-full flex justify-center items-center
+monitor_md:w-[70%]
+"
+                          >
+                            <AiOutlineLoading3Quarters className="animate-spin text-[1.5rem]" />
+                          </div>
+                        ) : (
+                          <>
+                            {update_trigger_schedule === true ? (
+                              <input
+                                type="datetime-local"
+                                required
+                                onChange={(e) =>
+                                  setAttendance_Schedule(e.target.value)
+                                }
+                                value={attendance_schedule}
+                                className="border-[1px] outline-none shadow-md shadow-slate-300
                             monitor_md:h-[1.6rem]
                             monitor_md:px-2
                             monitor_md:text-[14px]
                             monitor_md:w-[70%]
                             "
-                          />
-                        ) : (
-                          <span
-                            className=" flex flex-col justify-center
+                              />
+                            ) : (
+                              <span
+                                className=" flex flex-col justify-center
                             monitor_md:w-[70%]
                             monitor_md:h-full
                             "
-                          >
-                            <p
-                              className="font-semibold
+                              >
+                                <p
+                                  className="font-semibold
                             monitor_md:text-[14px]
                             "
-                            >
-                              {moment(attendance_schedule).format(
-                                "MMM DD, YYYY (HH:mm)"
-                              )}
-                            </p>
-                          </span>
+                                >
+                                  {moment(attendance_schedule).format(
+                                    "MMM DD, YYYY (HH:mm)"
+                                  )}
+                                </p>
+                              </span>
+                            )}
+                          </>
                         )}
                         <div className="w-[30%] flex justify-evenly items-center">
                           {update_trigger_schedule === true ? (
                             <>
-                              {loading_trigger_update_schedule === true ? (
-                                <>
-                                  <div className="w-full h-full flex justify-center items-center">
-                                    <AiOutlineLoading3Quarters className="text-[30px] animate-spin" />
-                                  </div>
-                                </>
-                              ) : (
-                                <>
-                                  <div className="w-full flex">
-                                    <div
-                                      className="w-full flex justify-evenly items-center
+                              <div className="w-full flex">
+                                <div
+                                  className="w-full flex justify-evenly items-center
                                       monitor_md:text-[1.2rem]
                                       "
-                                    >
-                                      <button
-                                        type="submit"
-                                        disabled={
-                                          loading_trigger_update_schedule ===
-                                          true
-                                            ? true
-                                            : false
-                                        }
-                                        className="flex justify-center items-center rounded-full border-[2px] border-green-600 text-green-600 transition-all ease-in-out duration-500 hover:bg-green-600 hover:text-white disabled:cursor-not-allowed
+                                >
+                                  <button
+                                    type="submit"
+                                    disabled={
+                                      loading_trigger_update_schedule === true
+                                        ? true
+                                        : false
+                                    }
+                                    className="flex justify-center items-center rounded-full border-[2px] border-green-600 text-green-600 transition-all ease-in-out duration-500 hover:bg-green-600 hover:text-white disabled:cursor-not-allowed
                                         monitor_md:h-[1.7rem]
                                         monitor_md:w-[1.7rem]
                                         monitor_md:text-[1rem]
                                         "
-                                      >
-                                        <IoIosSave />
-                                      </button>
-                                      <button
-                                        disabled={
-                                          loading_trigger_update_schedule ===
-                                          true
-                                            ? true
-                                            : false
-                                        }
-                                        onClick={() => {
-                                          setUpdate_Trigger_Schedule(false);
-                                        }}
-                                        className="flex justify-center items-center rounded-full border-[2px] border-red-600 text-red-600 transition-all ease-in-out duration-500 hover:bg-red-600 hover:text-white
+                                  >
+                                    <IoIosSave />
+                                  </button>
+                                  <button
+                                    disabled={
+                                      loading_trigger_update_schedule === true
+                                        ? true
+                                        : false
+                                    }
+                                    onClick={() => {
+                                      setUpdate_Trigger_Schedule(false);
+                                    }}
+                                    className="flex justify-center items-center rounded-full border-[2px] border-red-600 text-red-600 transition-all ease-in-out duration-500 hover:bg-red-600 hover:text-white
                                         monitor_md:h-[1.7rem]
                                         monitor_md:w-[1.7rem]
                                         monitor_md:text-[1rem]
                                         "
-                                      >
-                                        <FaTimesCircle />
-                                      </button>
-                                    </div>
-                                  </div>
-                                </>
-                              )}
+                                  >
+                                    <FaTimesCircle />
+                                  </button>
+                                </div>
+                              </div>
                             </>
                           ) : (
                             <div>
